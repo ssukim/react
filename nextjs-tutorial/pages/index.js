bc25d838adedc1b518cda495dcb6c8efbc0f00b6
ssukim <tntntn0930@gmail.com>
2021-06-21 09:03:28 +0900

    정적생성

--- nextjs-tutorial/pages/index.js
+++ nextjs-tutorial/pages/index.js
@@ -1,55 +1,90 @@
-import axios from "axios";
+
+import Axios from "axios";
 import Head from "next/head";
-import Image from "next/image";
 import { useEffect, useState } from "react";
-import styles from "../styles/Home.module.css";
-import ItemList from "../src/component/ItemList";
 import { Divider, Header, Loader } from "semantic-ui-react";
+import ItemList from "../src/component/ItemList";
 
-export default function Home() {
-  const [list, setList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+// export default function Home() {
+//   const [list, setList] = useState([]);
+//   const [isLoading, setIsLoading] = useState(true);
 
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+//   const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-  function getData() {
-    axios.get(API_URL).then((res) => {
-      // console.log(res.data);
-      setList(res.data);
-      setIsLoading(false);
-    });
-  }
+//   function getData() {
+//     axios.get(API_URL).then((res) => {
+//       // console.log(res.data);
+//       setList(res.data);
+//       setIsLoading(false);
+//     });
+//   }
 
-  useEffect(() => {
-    getData();
-  }, []);
+//   useEffect(() => {
+//     getData();
+//   }, []);
 
+//   return (
+//     <div>
+//       <Head>
+//         <title>Home | 쑤킴</title>
+//       </Head>
+//       {isLoading && (
+//         <div style={{padding: "300px 0"}}>
+//             <Loader inline="centered" active>
+//               Loading
+//             </Loader>
+//         </div>
+//       )}
+//       {!isLoading && (
+//         <>
+//           <Header as="h3" style={{ paddigTop: 40 }}>
+//             베스트 상품
+//           </Header>
+//           <Divider />
+//           <ItemList list={list.slice(0, 9)} />
+//           <Header as="h3" style={{ paddigTop: 40 }}>
+//             신상품
+//           </Header>
+//           <Divider />
+//           <ItemList list={list.slice(9)} />
+//         </>
+//       )}
+//     </div>
+//   );
+// }
+
+export default function Home({ list }) {
   return (
     <div>
       <Head>
-        <title>Home | 쑤킴</title>
+        <title>HOME | 코딩앙마</title>
+        <meta name="description" content="코딩 앙마 홈입니다."></meta>
       </Head>
-      {isLoading && (
-        <div style={{padding: "300px 0"}}>
-            <Loader inline="centered" active>
-              Loading
-            </Loader>
-        </div>
-      )}
-      {!isLoading && (
-        <>
-          <Header as="h3" style={{ paddigTop: 40 }}>
-            베스트 상품
-          </Header>
-          <Divider />
-          <ItemList list={list.slice(0, 9)} />
-          <Header as="h3" style={{ paddigTop: 40 }}>
-            신상품
-          </Header>
-          <Divider />
-          <ItemList list={list.slice(9)} />
-        </>
-      )}
+      <>
+        <Header as="h3" style={{ paddingTop: 40 }}>
+          베스트 상품
+        </Header>
+        <Divider />
+        <ItemList list={list.slice(0, 9)} />
+        <Header as="h3" style={{ paddingTop: 40 }}>
+          신상품
+        </Header>
+        <Divider />
+        <ItemList list={list.slice(9)} />
+      </>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const apiUrl = process.env.apiUrl;
+  const res = await Axios.get(apiUrl);
+  const data = res.data;
+
+  return {
+    props: {
+      list: data,
+      name: process.env.name,
+    },
+  };
 }
--- nextjs-tutorial/src/component/ItemList.js
+++ nextjs-tutorial/src/component/ItemList.js
@@ -1,5 +1,4 @@
 import { Grid } from "semantic-ui-react";
-import styles from "./ItemList.module.css";
 import styled from "styled-components";
 import Link from "next/link";
 
@@ -31,14 +30,41 @@ const ItemListBlock = styled.div`
   }
 `;
 
+// export default function ItemList({ list }) {
+//   return (
+//     <div>
+//       <Grid columns={3} divided>
+//         <Grid.Row>
+//           {list.map((item) => (
+//             <Grid.Column key={item.id}>
+//               <Link href={`/view/${item.id}`}>
+//                 <a>
+//                   <ItemListBlock>
+//                     <img src={item.image_link} alt={item.name} />
+//                     <strong>{item.name}</strong>
+//                     <span>
+//                       {item.category} {item.product_type}
+//                     </span>
+//                     <strong>${item.price}</strong>
+//                   </ItemListBlock>
+//                 </a>
+//               </Link>
+//             </Grid.Column>
+//           ))}
+//         </Grid.Row>
+//       </Grid>
+//     </div>
+//   );
+// }
+
 export default function ItemList({ list }) {
   return (
     <div>
-      <Grid columns={3} divided>
+      <Grid columns={3}>
         <Grid.Row>
           {list.map((item) => (
             <Grid.Column key={item.id}>
-              <Link href={`/view/${item.id}`}>
+              <Link href="/detail/[id]" as={`/detail/${item.id}`}>
                 <a>
                   <ItemListBlock>
                     <img src={item.image_link} alt={item.name} />
