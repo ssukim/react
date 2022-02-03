e7b863906de5406dec0a6ed3743299d50fe4d6e6
ssukim <tntntn0930@gmail.com>
2022-02-03 22:01:25 +0900

    저장소 정리

--- taskbox/.storybook/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  //👇 Location of our stories
-  stories: ['../src/components/**/*.stories.js'],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
--- taskbox/README.md
+++
@@ -1,5 +0,0 @@
-# StoryBook 연습
-StroyBook 튜토리얼을 보면서 왜 사용하는지 파악
-
-## tutorial URL
-https://storybook.js.org/tutorials/design-systems-for-developers/react/en/introduction/
